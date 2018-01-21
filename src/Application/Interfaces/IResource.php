a6f882de8c547cf29ae2bace2cc6ef35ebd22153
Mohamed Fawzy <mfawzy22@gmail.com>
2018-01-21 12:44:37 +0200

    init test cases

---
+++ src/Application/Interfaces/IResource.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Domain\Driven\Design\Application;
+class IResource
+{
+
+
+    public static function SayHello()
+    {
+        return "Hello world!";
+    }
+
+}
---
+++ tests/Application/Interfaces/IResourceTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Domain\Driven\Design\Application\IResource;
+
+class IResourceTest extends \PHPUnit\Framework\TestCase
+{
+    public function testFoo()
+    {
+        $this->assertEquals(5,5);
+    }
+
+}
